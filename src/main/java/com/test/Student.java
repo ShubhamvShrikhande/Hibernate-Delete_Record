8532f6cf99d8de0b7bf82853d8a3ee8c53c9bb8e
“ShubhamvShrikhandegit config --global user.email “shubhamvshri99@gmail.com <shubhamvshri99@gmail.com>
2023-05-24 18:13:36 +0530

    hibernate-delete_record

---
+++ src/main/java/com/test/Student.java
@@ -0,0 +1,56 @@
+package com.test;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="student")
+public class Student {
+	@Id
+	@Column(name ="id")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	@Column(name = "firstname")
+	private String firstname;
+	@Column(name="lastname")
+	private String lastname;
+	@Column(name ="city")
+	private String city;
+	@Column(name ="phoneNumber")
+	private String phoneNumber;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+}
